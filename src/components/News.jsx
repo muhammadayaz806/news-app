02ee9f9127f0ec50c06c14f1ba2c7fafbd0f6fa0
Muhammad Ayaz <ayazmughal806@gmail.com>
2026-03-06 15:17:41 +0500

    Video 25 Completed

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,13 +2,82 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  
+  articles = [
+      {
+        source: {
+          id: "bbc-sport",
+          name: "BBC Sport",
+        },
+        author: null,
+        title:
+          "T20 Cricket World Cup, India vs England: Harry Brook and Brendon McCullum need prime Jos Buttler in seismic semi-final",
+        description:
+          "England, Harry Brook and Brendon McCullum need Jos Buttler now as much as ever as they face reigning champions India in the T20 World Cup semi-final.",
+        url: "http://www.bbc.co.uk/sport/cricket/articles/c6266nmn8n7o",
+        urlToImage:
+          "https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/705f/live/a3defb20-17f2-11f1-801d-ed3cff6bf876.jpg",
+        publishedAt: "2026-03-04T19:22:33.1521523Z",
+        content:
+          "Mumbai has been celebrating the Hindu festival of Holi this week. The holiday packed with colour, music and parties marks the end of winter and the beginning of spring. On Thursday, attention will … [+1544 chars]",
+      },
+      {
+        source: {
+          id: "espn-cric-info",
+          name: "ESPN Cric Info",
+        },
+        author: null,
+        title:
+          "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+        description:
+          "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+        url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+        urlToImage:
+          "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+        publishedAt: "2020-04-27T11:41:47Z",
+        content:
+          "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+      },
+      {
+        source: {
+          id: "espn-cric-info",
+          name: "ESPN Cric Info",
+        },
+        author: null,
+        title:
+          "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        description:
+          "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        urlToImage:
+          "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        publishedAt: "2020-03-30T15:26:05Z",
+        content:
+          "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+      }
+  ];
+
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false
+    };
+  }
+
   render() {
     return (
       <div className="container my-3">
         <h2>Top Headlines</h2>
         <div className="row">
           <div className="col-md-4">
-            <NewsItem title={"Sample Title"} description={"Disc"} />
+            <NewsItem
+              title={"Sample Title"}
+              description={"Disc"}
+              imgUrl={
+                "https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/705f/live/a3defb20-17f2-11f1-801d-ed3cff6bf876.jpg"
+              }
+            />
           </div>
           <div className="col-md-4">
             <NewsItem title={"Sample Title"} description={"Disc"} />
